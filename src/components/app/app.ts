1c353d1bcca479886edd6c4a2689b06566421699
Simon Liljeström <simon.liljestrom@wartsila.com>
2018-05-08 10:59:48 +0300

    Closes #1 updated font-awesome to version 5

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,6 +1,17 @@
 import { Aurelia, PLATFORM } from "aurelia-framework";
 import { Router, RouterConfiguration } from "aurelia-router";
-import 'font-awesome/css/font-awesome.css';
+
+// import entire folders if you want like this:
+
+// import solid from '@fortawesome/fontawesome-free-solid';
+// import regular from '@fortawesome/fontawesome-free-regular';
+// import brands from '@fortawesome/fontawesome-free-brands';
+
+import fontawesome from '@fortawesome/fontawesome';
+import { faHome, faPlus, faThList } from '@fortawesome/fontawesome-free-solid';
+
+// Pre-registering icon definitions so that you do not have to explicitly pass them to render an icon.
+fontawesome.library.add(faHome, faPlus, faThList, faThList, /*solid, etc..*/);
 
 export class App {
 	configureRouter(config: RouterConfiguration, router: Router) {
@@ -8,21 +19,22 @@ export class App {
 		config.map([{
 			route: ["", "home"],
 			name: "home",
-			settings: { icon: 'home' },
+			settings: { icon: faHome.iconName, prefix: faHome.prefix },
+			fontPrefix: 'fa',
 			moduleId: PLATFORM.moduleName("../../components/pages/home/home"),
 			nav: true,
 			title: "Home"
 		}, {
 			route: "counter",
 			name: "counter",
-			settings: { icon: "plus" },
+			settings: { icon: faPlus.iconName, prefix: faPlus.prefix },
 			moduleId: PLATFORM.moduleName("../../components/pages/counter/counter"),
 			nav: true,
 			title: "Counter"
 		}, {
 			route: "fetch-data",
 			name: "fetchdata",
-			settings: { icon: "th-list" },
+			settings: { icon: faThList.iconName, prefix: faThList.prefix, },
 			moduleId: PLATFORM.moduleName("../../components/pages/fetchdata/fetchdata"),
 			nav: true,
 			title: "Fetch"
