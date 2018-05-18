c9f6fbbefff8f3c8b84766b7bcc3d5d8ef33ace3
greensponge <greensponge@gmail.com>
2018-05-18 23:19:39 +0300

    removed unused using statements

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AureliaDotnetTemplate.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
