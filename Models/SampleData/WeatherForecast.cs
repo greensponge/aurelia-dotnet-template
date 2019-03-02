5f016d0523cde71aa0e27993261c4f0246ec35ca
greensponge <greensponge@gmail.com>
2019-03-02 17:14:39 +0200

    updated packages, made weatherforecast a little less verbose

--- Models/SampleData/WeatherForecast.cs
+++ Models/SampleData/WeatherForecast.cs
@@ -3,15 +3,8 @@
     public class WeatherForecast
     {
         public string DateFormatted { get; set; }
-        public int TemperatureC { get; set; }
         public string Summary { get; set; }
-
-        public int TemperatureF
-        {
-            get
-            {
-                return 32 + (int)(this.TemperatureC / 0.5556);
-            }
-        }
+        public int TemperatureC { get; set; }
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }
